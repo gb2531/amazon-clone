58482d53c3e46de5ddb62132685898af4ed54967
gb2531 <balasurya1998@gmail.com>
2024-05-14 20:47:31 +0530

    added header to home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+const routes: Routes = [
+  {path:"home" , component:HomeComponent},
+  {path:"header", component:HeaderComponent},
+  {path:"footer", component:FooterComponent},
+{path:"", redirectTo:"home", pathMatch:"full"}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
